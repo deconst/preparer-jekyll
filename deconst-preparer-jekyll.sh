e216f6acdf62bb3407320260393e428b5a5f449f
Ash Wilson <smashwilson@gmail.com>
2015-05-04 09:10:34 -0400

    Runner script

---
+++ deconst-preparer-jekyll.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SUDO=sudo
+
+which boot2docker >/dev/null 2>&1 && {
+  SUDO=
+  CONTENT_STORE_URL=${CONTENT_STORE_URL:-http://$(boot2docker ip):9000/}
+}
+
+CONTENT_STORE_URL=${CONTENT_STORE_URL:-http://localhost:9000/}
+WORKDIR=${1:-$(pwd)}
+
+[ -z "${CONTENT_ID_BASE}" ] && {
+  cat <<EOM 1>&2
+Please specify a content ID base:
+
+  export CONTENT_ID_BASE=https://github.com/myname/myrepo
+
+Our convention is to use the base URL of your GitHub repo.
+EOM
+  exit 1
+}
+
+${SUDO} docker run \
+  --rm=true \
+  -e CONTENT_STORE_URL=${CONTENT_STORE_URL} \
+  -e CONTENT_ID_BASE=${CONTENT_ID_BASE} \
+  -e TRAVIS_PULL_REQUEST="false" \
+  -v ${WORKDIR}:/usr/control-repo \
+  quay.io/deconst/preparer-jekyll
