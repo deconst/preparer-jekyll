fe6d888803f51726c8b97974152e6b6ec73389ff
Ash Wilson <smashwilson@gmail.com>
2015-05-07 14:01:45 -0400

    Run "bundle install" if a Gemfile is present.

--- lib/preparermd.rb
+++ lib/preparermd.rb
@@ -1,5 +1,8 @@
 require "jekyll"
 require "jekyll-assets"
+require "bundler"
+require "bundler/cli"
+require "bundler/cli/install"
 
 require "preparermd/version"
 require "preparermd/config"
@@ -15,6 +18,12 @@ module PreparerMD
     @config = Config.new
     @config.validate
 
+    if File.exist?("Gemfile")
+      puts "Installing dependencies from the Jekyll environment."
+      Bundler::CLI::Install.new({}).run
+    end
+
+    puts "Building and submitting content."
     Jekyll::Commands::Build.process({})
   end
 
