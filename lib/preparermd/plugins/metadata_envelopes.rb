920fe00fbe26fe19eba2d5106edef3d34b731ba5
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-08-12 06:59:46 -0500

    javascript changed me

--- lib/preparermd/plugins/metadata_envelopes.rb
+++ lib/preparermd/plugins/metadata_envelopes.rb
@@ -38,7 +38,7 @@ module PreparerMD
 
       output = page.to_liquid
 
-      envelope = page.data;
+      envelope = page.data
 
       envelope["title"] = output["title"]
       envelope["body"] = output["content"]
