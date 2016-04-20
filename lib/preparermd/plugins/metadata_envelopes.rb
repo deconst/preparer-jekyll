0999e8acaf24fa6e7c783cf630caf14c90c66a05
Ash Wilson <smashwilson@gmail.com>
2016-04-20 11:59:17 -0400

    Account for multiple asset occurrences.
    Fixes #36.

--- lib/preparermd/plugins/metadata_envelopes.rb
+++ lib/preparermd/plugins/metadata_envelopes.rb
@@ -164,11 +164,11 @@ module PreparerMD
       envelope["meta"] = meta
 
       # Derive asset offsets from the rendered envelope
-      asset_offsets = {}
+      asset_offsets = Hash.new { |h, k| h[k] = [] }
       adjustment = 0
       envelope[:body].gsub! /__deconst-asset:([^_]+)__/ do
         md = Regexp.last_match
-        asset_offsets[URI.unescape md[1]] = md.begin(0) + adjustment
+        asset_offsets[URI.unescape md[1]] << md.begin(0) + adjustment
         adjustment -= (md[0].length - 1)
         'X'
       end
